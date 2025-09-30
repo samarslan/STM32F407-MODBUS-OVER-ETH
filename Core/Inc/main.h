dca287c7707aea87b4d06544547460d1b7c54c3a
samarslan <86328409+samarslan@users.noreply.github.com>
2025-09-30 15:10:41 +0300

    cube mx spi code generation

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -72,6 +72,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define SPI_CS_Pin GPIO_PIN_4
+#define SPI_CS_GPIO_Port GPIOA
 
 /* USER CODE BEGIN Private defines */
 extern UART_HandleTypeDef huart4;
